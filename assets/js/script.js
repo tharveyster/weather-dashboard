a6378509fd8f3c731450dd9332938d9bf354e1f6
tharvey <tharveyster@gmail.com>
2021-06-24 10:40:59 -0400

    Added condition icon and UV index color code to current conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var APIKey = "77d9b213504a58408f3d89d57b484a0d";
 var city;
 var cityNameSearch = document.querySelector("#cityName");
 var searchButton = document.querySelector("#citySearch");
-var currentDate = moment().format('YYYY-MM-DD');
+var currentDate = moment().format('M/D/YYYY');
 console.log(currentDate);
 
 function getWeather() {
@@ -18,7 +18,11 @@ function getWeather() {
             var wind = document.createElement('p');
             var humidity = document.createElement('p');
             var uvIndex = document.createElement('p');
-            cityName.innerHTML = data.name;
+            var uvNumber = document.createElement('span');
+            var todayIcon = (data.weather[0].icon);
+            var todayIconImage = '<img src="http://openweathermap.org/img/wn/' + todayIcon + '.png" alt="" />';
+            console.log(todayIconImage);
+            cityName.innerHTML = data.name + ' (' + currentDate + ')' + todayIconImage;
             temp.innerHTML = 'Temp: ' + (data.main.temp) + '&#176F';
             wind.innerHTML = 'Wind: ' + (data.wind.speed) + ' MPH';
             humidity.innerHTML = 'Humidity: ' + (data.main.humidity) + '%';
@@ -36,7 +40,20 @@ function getWeather() {
                 .then(function (data) {
                     console.log(data);
                     var uviValue = data.current.uvi;
-                    uvIndex.innerHTML = 'UV Index: ' + uviValue;
+                    if (uviValue <= 2.99) {
+                        $(uvNumber).addClass('uvIndex green');
+                    } else if (uviValue >= 3 && uviValue <= 5.99) {
+                        $(uvNumber).addClass('uvIndex yellow');
+                    } else if (uviValue >= 6 && uviValue <= 7.99) {
+                        $(uvNumber).addClass('uvIndex orange');
+                    } else if (uviValue >= 8 && uviValue <= 10.99) {
+                        $(uvNumber).addClass('uvIndex red');
+                    } else {
+                        $(uvNumber).addClass('uvIndex violet');
+                    }
+                    uvNumber.innerHTML = uviValue;
+                    uvIndex.textContent = 'UV Index: ';
+                    uvIndex.append(uvNumber);
                     weatherBlock.append(uvIndex);
                 })
         })
